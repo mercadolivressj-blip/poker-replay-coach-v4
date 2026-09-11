e8b5877c373234af45cdb84ed65cb46af7d1ea0e
mercadolivressj-blip <mercadolivressj@gmail.com>
2026-09-11 16:40:39 -0300

    R8: add isolated card-stability bootstrap

---
+++ src/bootstrap-r8.js
@@ -0,0 +1,5 @@
+import './core/runtime-guards.js';
+await import('./main.js');
+await import('./vision/replay-lifecycle-r8.js');
+await import('./vision/card-refiner-runtime.js');
+await import('./solver/resolver-runtime.js');
