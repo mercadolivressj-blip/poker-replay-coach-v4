cd71a7c5a4f1f4ea5e65e823c72f691e0cfc1ba2
mercadolivressj-blip <mercadolivressj@gmail.com>
2026-09-11 00:37:50 -0300

    Feed local Dealer actions into continual resolver

--- src/bootstrap.js
+++ src/bootstrap.js
@@ -1,4 +1,5 @@
 import './core/runtime-guards.js';
 await import('./main.js');
 await import('./coach/coach-runtime.js');
+await import('./vision/local-action-runtime.js');
 await import('./solver/resolver-runtime.js');
